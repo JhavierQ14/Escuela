91a3970496678b317db1852ad4c4ff74171b9e89
carlosjavierjimenezvasquez <69540480+carlosjavierjimenezvasquez@users.noreply.github.com>
2021-09-30 11:30:25 -0600

    updateClas30/9

--- Escuela/Startup.cs
+++ Escuela/Startup.cs
@@ -1,9 +1,12 @@
+using Escuela.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +27,9 @@ namespace Escuela
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+           services.AddDbContext<ApplicationDbContext>(Options =>
+                Options.UseSqlServer(Configuration.GetConnectionString
+                ("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
